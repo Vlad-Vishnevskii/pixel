67a25728d63837401c291c499bd4d051f08c313d
vlad-linux-home-pc <v.vishnevskii@idynsys.org>
2023-12-15 15:43:33 +0300

    initial commit

---
+++ src/landing/Landing.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Header } from './components';
+import './Landing.styles.scss';
+
+export const Landing = () => {
+  return (
+    <div className="landing">
+      <Header />
+    </div>
+  );
+};
---
+++ src/landing/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.style.scss';
+
+export const Header = () => {
+  return (
+    <div className="header">
+      <img alt="Pixel" />
+
+      <h1>Курс Иллюстрации</h1>
+    </div>
+  );
+};
