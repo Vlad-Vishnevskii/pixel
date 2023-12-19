04d60e212d7ec5f854d0734151b198f75c925817
Olga Polyanskaya <151590@mail.ru>
2023-12-19 22:53:19 +0300

    правит ошибки в консоли

--- src/landing/components/Video/assets/IconPlay.js
+++ src/landing/components/Video/assets/IconPlay.js
@@ -9,7 +9,7 @@ export const IconPlay = () => {
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <circle cx="22" cy="22.2168" r="22" fill="black" fill-opacity="0.4" />
+      <circle cx="22" cy="22.2168" r="22" fill="black" fillOpacity="0.4" />
       <path
         d="M35 22.2168L15.5 33.4751L15.5 10.9585L35 22.2168Z"
         fill="white"
