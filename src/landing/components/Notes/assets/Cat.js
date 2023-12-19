3785c8c69ad540b032a6cd6f4c53c4547ab1c84b
Olga Polyanskaya <151590@mail.ru>
2023-12-19 23:56:22 +0300

    добавляет блок конспекты

---
+++ src/landing/components/Notes/assets/Cat.js
@@ -0,0 +1,154 @@
+import React from 'react';
+
+export const Cat = () => {
+  return (
+    <svg
+      width={110}
+      height={95}
+      viewBox="0 0 110 95"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M30.4342 74.7734C30.4342 74.7734 23.2694 94.3796 52.3583 94.1965C81.4471 94.0133 97.4323 73.625 91.6835 63.2618C86.6745 54.2231 30.9767 60.8102 30.4342 74.7734Z"
+        fill="#EF6843"
+      />
+      <path
+        d="M29.7505 84.4326C31.5822 89.35 37.1266 94.19 52.0409 94.19C69.4562 94.19 80.2069 87.8142 88.1395 79.2827"
+        stroke="#414042"
+        strokeMiterlimit={10}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M90.8096 75.4714C90.9293 75.1967 91.0491 74.9149 91.1689 74.6401"
+        stroke="#414042"
+        strokeMiterlimit={10}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M71.8798 0.681641C69.3365 0.681641 62.679 13.1654 63.3342 31.3556L88.4144 33.0887C88.4144 33.0887 80.3549 0.681641 71.8798 0.681641Z"
+        fill="white"
+        stroke="#414042"
+        strokeMiterlimit={10}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M32.7309 0.681298C30.2511 1.23081 23.5302 13.1651 24.1853 31.3553L46.3771 20.8793C46.3771 20.8793 37.2186 -0.319093 32.7309 0.681298Z"
+        fill="white"
+        stroke="#414042"
+        strokeMiterlimit={10}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M97.1926 55.4494C95.0298 71.3148 75.4446 84.8834 52.964 84.8834C35.6051 84.8834 21.1206 76.204 15.4916 65.0729C13.1174 60.3739 12.6806 54.9281 13.991 49.8275C17.9433 34.4694 29.4478 17.2725 55.0775 18.2588C87.6677 19.5199 97.1926 40.838 97.1926 55.4494Z"
+        fill="white"
+      />
+      <path
+        d="M52.957 84.883C69.8157 84.883 85.0471 77.2533 92.5218 66.7421C95.0157 63.2407 96.6431 59.4153 97.1856 55.449C97.5449 45.7057 94.5296 38.1464 87.224 30.6294"
+        stroke="#414042"
+        strokeMiterlimit={10}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M32.3081 8.50819C34.0623 8.10663 36.2885 14.0174 38.1625 18.3008C38.6345 19.3857 38.1061 20.6397 37.0212 21.0483C37.0212 21.0483 31.4697 23.7888 27.3977 27.2409C19.4862 33.9477 15.3014 42.7469 13.4556 51.4193C13.0893 53.1454 12.9272 56.0056 13.1668 57.7528C14.7167 69.0882 25.4462 79.381 40.1914 83.2557"
+        stroke="#414042"
+        strokeMiterlimit={10}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M78.2983 23.9509C78.2983 23.9509 76.1425 8.77594 71.9155 8.49414"
+        stroke="#414042"
+        strokeMiterlimit={10}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M67.2578 19.8936C70.6465 20.7178 74.014 21.8802 78.2973 23.9515"
+        stroke="#414042"
+        strokeMiterlimit={10}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M44.4961 18.9704C47.7227 18.3716 51.2382 18.1109 55.0777 18.2589C56.691 18.3223 59.8049 18.3786 63.9967 19.1677"
+        stroke="#414042"
+        strokeMiterlimit={10}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M7.03053 54.0687L1 53.7939"
+        stroke="#414042"
+        strokeMiterlimit={10}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M7.18545 60.1343L1.18311 61.3319"
+        stroke="#414042"
+        strokeMiterlimit={10}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M102.448 54.0687L108.479 53.7939"
+        stroke="#414042"
+        strokeMiterlimit={10}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M102.293 60.1343L109 61.3319"
+        stroke="#414042"
+        strokeMiterlimit={10}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M29.0459 81.4102L29.0741 81.6004"
+        stroke="#414042"
+        strokeMiterlimit={10}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M42.5588 56.1044C41.7064 54.8223 41.805 53.688 43.3056 53.3498C44.5737 53.061 47.5255 53.2724 48.6598 53.5964C50.1533 54.0262 50.731 55.1393 49.2938 56.654C47.9835 58.0348 46.4265 58.7956 46.4265 58.7956C46.0038 59.0915 45.4332 59.0634 45.0387 58.7252C45.0457 58.7252 43.3338 57.2669 42.5588 56.1044Z"
+        fill="#414042"
+      />
+      <path
+        d="M34.8516 60.8456C35.2038 61.9658 36.7255 62.621 38.1627 62.6773C40.5369 62.7689 42.9463 60.7329 45.6727 60.7329C47.1451 60.7329 48.5612 61.1486 49.9208 61.6065C51.2805 62.0644 52.6754 62.5505 54.1478 62.5717C55.6203 62.5928 57.2124 62.0362 57.7901 60.9443"
+        stroke="#414042"
+        strokeMiterlimit={10}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M45.757 55.4424C45.757 55.4424 45.447 59.141 45.9683 60.4021"
+        stroke="#414042"
+        strokeMiterlimit={10}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M67.7868 49.0665C65.2154 46.5162 59.2624 47.6787 57.4307 50.4333"
+        stroke="#414042"
+        strokeMiterlimit={10}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M26.2847 49.0665C28.8561 46.5162 34.8091 47.6787 36.6408 50.4333"
+        stroke="#414042"
+        strokeMiterlimit={10}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
