67b37aab84bc938be8227c959f5dbeb0f6c7489d
vishnevskii.vlad <v.vishnevskii@idynsys.org>
2023-12-21 08:34:50 +0300

    адаптивная полоска и пп блока программа

--- src/landing/components/Program/AccordionList/AccordionList.constans.js
+++ src/landing/components/Program/AccordionList/AccordionList.constans.js
@@ -2,26 +2,51 @@ export const PROGRAM_LIST = [
   {
     id: 1,
     name: 'Рисование иконок',
-    bg: '',
+    bg: './images/landingContent/program/program-1.png',
   },
   {
     id: 2,
     name: 'Работа с пером',
-    bg: '',
+    bg: './images/landingContent/program/program-2.png',
   },
   {
     id: 3,
     name: 'Цвет и тон',
-    bg: '',
+    bg: './images/landingContent/program/program-3.png',
   },
   {
     id: 4,
     name: 'Светотень и композиция',
-    bg: '',
+    bg: './images/landingContent/program/program-4.png',
   },
   {
     id: 5,
     name: 'Рисование персонажей',
-    bg: '',
+    bg: './images/landingContent/program/program-5.png',
+  },
+  {
+    id: 6,
+    name: 'Градиент и маски',
+    bg: './images/landingContent/program/program-6.png',
+  },
+  {
+    id: 7,
+    name: 'Кисти и шумы',
+    bg: './images/landingContent/program/program-7.png',
+  },
+  {
+    id: 8,
+    name: 'Изометрия',
+    bg: './images/landingContent/program/program-8.png',
+  },
+  {
+    id: 9,
+    name: 'Оформление работы',
+    bg: './images/landingContent/program/program-9.png',
+  },
+  {
+    id: 10,
+    name: 'Оформление работы',
+    bg: './images/landingContent/program/program-1.png',
   },
 ];
