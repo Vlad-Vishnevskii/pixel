83573352533152b10c82d816a466adaebe66424a
vishnevskii.vlad <v.vishnevskii@idynsys.org>
2023-12-22 14:58:09 +0300

    добавляет рефакторинг формы и раскрытие одного блока картинок

--- src/landing/components/Works/Works.js
+++ src/landing/components/Works/Works.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Works.style.scss';
 import { Button } from './../Button/Button';
 import { WORKS_DATA } from './Works.constants';
 import { Star } from './assets/Star';
 
+const ITERATION_SHOW_COUNT = 8;
+
 export const Works = () => {
+  const [worksLength, setWorksLength] = useState(ITERATION_SHOW_COUNT);
+
+  const showMoreHanlder = () => {
+    setWorksLength((prev) => prev + (WORKS_DATA.length - ITERATION_SHOW_COUNT));
+  };
+
   return (
     <section className="works">
       <div className="container">
@@ -15,8 +23,8 @@ export const Works = () => {
           <div className="works_decor"></div>
           <h2 className="works_title">Работы выпускников</h2>
           <ul className="works_list">
-            {WORKS_DATA.map((item) => (
-              <li className={`works_item ${item.className}`}>
+            {WORKS_DATA.slice(0, worksLength).map((item, index) => (
+              <li key={index} className={`works_item ${item.className}`}>
                 <div className="works_imgWrapper">
                   <img
                     src={item.img}
@@ -28,11 +36,14 @@ export const Works = () => {
               </li>
             ))}
           </ul>
-          <Button
-            className="button--white"
-            text="Посмотреть ещё"
-            type="button"
-          />
+          {worksLength < WORKS_DATA.length && (
+            <Button
+              className="button--white"
+              text="Посмотреть ещё"
+              type="button"
+              onClick={showMoreHanlder}
+            />
+          )}
         </div>
       </div>
     </section>
