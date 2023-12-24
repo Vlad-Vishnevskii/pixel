52b7c0126fd726701a32af9f70706baaa47b0b04
vishnevskii.vlad <v.vishnevskii@idynsys.org>
2023-12-25 01:01:44 +0300

    Добавляет функционал кнопки еще в портфолио и фрейм для просмотра изображении

---
+++ src/landing/components/Portfolio/Portfolio.constans.js
@@ -0,0 +1,30 @@
+export const PORTFOLIO_LIST = [
+  {
+    portfolio: './images/landingContent/portfolio/portfolio-1.png',
+    avatar: './images/landingContent/portfolio/student.jpg',
+    name: 'Яна Гусева',
+  },
+  {
+    portfolio: './images/landingContent/portfolio/portfolio-1.png',
+    avatar: './images/landingContent/portfolio/student.jpg',
+    name: 'Яна Гусева',
+  },
+  {
+    portfolio: './images/landingContent/portfolio/portfolio-1.png',
+    avatar: './images/landingContent/portfolio/student.jpg',
+    name: 'Яна Гусева',
+  },
+  {
+    portfolio: './images/landingContent/portfolio/portfolio-1.png',
+    avatar: './images/landingContent/portfolio/student.jpg',
+    name: 'Яна Гусева',
+  },
+  {
+    portfolio: './images/landingContent/portfolio/portfolio-1.png',
+    avatar: './images/landingContent/portfolio/student.jpg',
+    name: 'Яна Гусева',
+  },
+];
+
+export const INITIAL_SHOW_COUNT = 1;
+export const ITERATION_SHOW_COUNT = 1;
--- src/landing/components/Portfolio/Portfolio.images.js
+++
@@ -1,4 +0,0 @@
-export const PORTFOLIO_IMAGES = {
-  PORTFOLIO1: './images/landingContent/portfolio/portfolio-1.png',
-  STUDENT: './images/landingContent/portfolio/student.jpg',
-};
