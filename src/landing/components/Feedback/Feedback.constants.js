997d8f9fdfd68a842f92167eca23577fbf6ab04f
vishnevskii.vlad <v.vishnevskii@idynsys.org>
2023-12-27 00:01:51 +0300

    Добавляет работы в до-после

--- src/landing/components/Feedback/Feedback.constants.js
+++ src/landing/components/Feedback/Feedback.constants.js
@@ -2,37 +2,100 @@ export const FEEDBACK_IMAGES = [
   {
     name: 'Работа Александры Гальчевской',
     img1: {
-      imageUrl: './images/landingContent/feedback/a-galchevskya-1.jpg',
+      imageUrl: './images/landingContent/feedback/galchevskaya-1.jpg',
     },
     img2: {
-      imageUrl: './images/landingContent/feedback/a-galchevskya-2.jpg',
+      imageUrl: './images/landingContent/feedback/galchevskaya-2.jpg',
     },
   },
   {
     name: 'Работа Анастасии Раннель',
     img1: {
-      imageUrl: './images/landingContent/feedback/a-rannel-1.jpg',
+      imageUrl: './images/landingContent/feedback/ranel-1.jpg',
     },
     img2: {
-      imageUrl: './images/landingContent/feedback/a-rannel-2.jpg',
+      imageUrl: './images/landingContent/feedback/ranel-2.jpg',
     },
   },
   {
     name: 'Работа Евгении Петуховой',
     img1: {
-      imageUrl: './images/landingContent/feedback/e.petuhova-1.jpg',
+      imageUrl: './images/landingContent/feedback/petuhova-1.png',
     },
     img2: {
-      imageUrl: './images/landingContent/feedback/e.petuhova-2.jpg',
+      imageUrl: './images/landingContent/feedback/petuhova-2.png',
     },
   },
   {
     name: 'Работа Крестины Жук',
     img1: {
-      imageUrl: './images/landingContent/feedback/k-juk-1.jpg',
+      imageUrl: './images/landingContent/feedback/juk-1.png',
     },
     img2: {
-      imageUrl: './images/landingContent/feedback/k-juk-2.jpg',
+      imageUrl: './images/landingContent/feedback/juk-2.png',
+    },
+  },
+  {
+    name: 'Работа Руслана Ампилова',
+    img1: {
+      imageUrl: './images/landingContent/feedback/ampilov-1.jpg',
+    },
+    img2: {
+      imageUrl: './images/landingContent/feedback/ampilov-2.jpg',
+    },
+  },
+  {
+    name: 'Работа Татьяны Артамоновой',
+    img1: {
+      imageUrl: './images/landingContent/feedback/artamonova-1.jpg',
+    },
+    img2: {
+      imageUrl: './images/landingContent/feedback/artamonova-2.jpg',
+    },
+  },
+  {
+    name: 'Работа Ольги Брауловой',
+    img1: {
+      imageUrl: './images/landingContent/feedback/braulova-1.jpg',
+    },
+    img2: {
+      imageUrl: './images/landingContent/feedback/braulova-2.jpg',
+    },
+  },
+  {
+    name: 'Работа Ксюши Каравашкиной',
+    img1: {
+      imageUrl: './images/landingContent/feedback/karavashkina-1.png',
+    },
+    img2: {
+      imageUrl: './images/landingContent/feedback/karavashkina-2.png',
+    },
+  },
+  {
+    name: 'Работа Альбины Каримовой',
+    img1: {
+      imageUrl: './images/landingContent/feedback/karimova-1.png',
+    },
+    img2: {
+      imageUrl: './images/landingContent/feedback/karimova-2.png',
+    },
+  },
+  {
+    name: 'Работа Марии Новиковой',
+    img1: {
+      imageUrl: './images/landingContent/feedback/novikova-1.png',
+    },
+    img2: {
+      imageUrl: './images/landingContent/feedback/novikova-2.png',
+    },
+  },
+  {
+    name: 'Работа Милы Якушиной',
+    img1: {
+      imageUrl: './images/landingContent/feedback/yakushina-1.jpg',
+    },
+    img2: {
+      imageUrl: './images/landingContent/feedback/yakushina-2.jpg',
     },
   },
 ];
