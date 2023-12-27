ea74253a89a2c8b9762a7a18edcbe1801c15d946
Olga Polyanskaya <151590@mail.ru>
2023-12-27 19:18:47 +0300

    дообавляет ссылки

--- src/landing/components/Socials/Socials.constans.js
+++ src/landing/components/Socials/Socials.constans.js
@@ -6,17 +6,20 @@ export const SOCIALS_TEXT_CONTENT = [
     src: SOCIALS_IMAGES.VK,
     alt: 'Вконтакте',
     ariaLabel: 'Перейти на страницу Вконтакте',
+    href: "https://vk.com/pixel_ai",
   },
   {
     id: 1,
     src: SOCIALS_IMAGES.YOUTUBE,
     alt: 'Youtube',
     ariaLabel: 'Перейти на страницу Youtube',
+    href: "https://www.youtube.com/@pixelone",
   },
   {
     id: 2,
     src: SOCIALS_IMAGES.INSTAGRAM,
     alt: 'Instagram',
     ariaLabel: 'Перейти на страницу Instagram',
+    href: "https://www.instagram.com/pixel.blog/?hl=en",
   },
 ];
