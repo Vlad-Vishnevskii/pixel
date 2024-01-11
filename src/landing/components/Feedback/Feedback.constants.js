effed624710feb76d5ca6e3cc849f4ad74343938
Vlad <vishnevskii_vlad@mail.ru>
2024-01-11 20:24:18 +0300

    обновляет картинки

--- src/landing/components/Feedback/Feedback.constants.js
+++ src/landing/components/Feedback/Feedback.constants.js
@@ -2,100 +2,100 @@ export const FEEDBACK_IMAGES = [
   {
     name: 'Работа Александры Гальчевской',
     img1: {
-      imageUrl: './images/landingContent/feedback/galchevskaya-1.webp',
+      imageUrl: './images/landingContent/feedback/galchevskaya-1.jpg',
     },
     img2: {
-      imageUrl: './images/landingContent/feedback/galchevskaya-2.webp',
+      imageUrl: './images/landingContent/feedback/galchevskaya-2.jpg',
     },
   },
   {
     name: 'Работа Анастасии Раннель',
     img1: {
-      imageUrl: './images/landingContent/feedback/ranel-1.webp',
+      imageUrl: './images/landingContent/feedback/ranel-1.jpg',
     },
     img2: {
-      imageUrl: './images/landingContent/feedback/ranel-2.webp',
+      imageUrl: './images/landingContent/feedback/ranel-2.jpg',
     },
   },
   {
     name: 'Работа Евгении Петуховой',
     img1: {
-      imageUrl: './images/landingContent/feedback/petuhova-1.webp',
+      imageUrl: './images/landingContent/feedback/petuhova-1.jpg',
     },
     img2: {
-      imageUrl: './images/landingContent/feedback/petuhova-2.webp',
+      imageUrl: './images/landingContent/feedback/petuhova-2.jpg',
     },
   },
   {
     name: 'Работа Крестины Жук',
     img1: {
-      imageUrl: './images/landingContent/feedback/juk-1.webp',
+      imageUrl: './images/landingContent/feedback/juk-1.jpg',
     },
     img2: {
-      imageUrl: './images/landingContent/feedback/juk-2.webp',
+      imageUrl: './images/landingContent/feedback/juk-2.jpg',
     },
   },
   {
     name: 'Работа Руслана Ампилова',
     img1: {
-      imageUrl: './images/landingContent/feedback/ampilov-1.webp',
+      imageUrl: './images/landingContent/feedback/ampilov-1.jpg',
     },
     img2: {
-      imageUrl: './images/landingContent/feedback/ampilov-2.webp',
+      imageUrl: './images/landingContent/feedback/ampilov-2.jpg',
     },
   },
   {
     name: 'Работа Татьяны Артамоновой',
     img1: {
-      imageUrl: './images/landingContent/feedback/artamonova-1.webp',
+      imageUrl: './images/landingContent/feedback/artamonova-1.jpg',
     },
     img2: {
-      imageUrl: './images/landingContent/feedback/artamonova-2.webp',
+      imageUrl: './images/landingContent/feedback/artamonova-2.jpg',
     },
   },
   {
     name: 'Работа Ольги Брауловой',
     img1: {
-      imageUrl: './images/landingContent/feedback/braulova-1.webp',
+      imageUrl: './images/landingContent/feedback/braulova-1.jpg',
     },
     img2: {
-      imageUrl: './images/landingContent/feedback/braulova-2.webp',
+      imageUrl: './images/landingContent/feedback/braulova-2.jpg',
     },
   },
   {
     name: 'Работа Ксюши Каравашкиной',
     img1: {
-      imageUrl: './images/landingContent/feedback/karavashkina-1.webp',
+      imageUrl: './images/landingContent/feedback/karavashkina-1.jpg',
     },
     img2: {
-      imageUrl: './images/landingContent/feedback/karavashkina-2.webp',
+      imageUrl: './images/landingContent/feedback/karavashkina-2.jpg',
     },
   },
   {
     name: 'Работа Альбины Каримовой',
     img1: {
-      imageUrl: './images/landingContent/feedback/karimova-1.webp',
+      imageUrl: './images/landingContent/feedback/karimova-1.jpg',
     },
     img2: {
-      imageUrl: './images/landingContent/feedback/karimova-2.webp',
+      imageUrl: './images/landingContent/feedback/karimova-2.jpg',
     },
   },
   {
     name: 'Работа Марии Новиковой',
     img1: {
-      imageUrl: './images/landingContent/feedback/novikova-1.webp',
+      imageUrl: './images/landingContent/feedback/novikova-1.jpg',
     },
     img2: {
-      imageUrl: './images/landingContent/feedback/novikova-2.webp',
+      imageUrl: './images/landingContent/feedback/novikova-2.jpg',
     },
   },
   {
     name: 'Работа Милы Якушиной',
     img1: {
-      imageUrl: './images/landingContent/feedback/yakushina-1.webp',
+      imageUrl: './images/landingContent/feedback/yakushina-1.jpg',
     },
     img2: {
-      imageUrl: './images/landingContent/feedback/yakushina-2.webp',
+      imageUrl: './images/landingContent/feedback/yakushina-2.jpg',
     },
   },
 ];
