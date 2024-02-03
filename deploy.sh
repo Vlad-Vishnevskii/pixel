352195c12649cf63ccf59aea3e8cc44e16c32eb2
Evgeny Kuizhev <ekuizhev@gmail.com>
2024-02-03 11:14:51 +0300

    update

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-ssh pixel@pixel.one -p 4227 << EOF
-  cd /srv/adobe-illustrator-landing/
-  npm i
-  npm run build
-  exit
+ssh pixel@pixel.one -p 4227 /bin/bash << EOF
+    source ~/.bashrc
+    source ~/.nvm/nvm.sh
+    nvm use 14.4.0
+    cd /srv/adobe-illustrator-landing/
+    git pull --rebase origin HEAD
+    npm install && npm run build
+    echo "Ok"
 EOF
